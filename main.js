edcd26138d9cd2ebc3ab75a3d919b2797e0efcc6
walleser-karli <k.walleser@ufl.edu>
2021-06-06 16:28:25 -0400

    Set up pizza variable equal to an object with three properties and one method

---
+++ main.js
@@ -0,0 +1,11 @@
+var pizza = {
+    crust: "thin",
+    cheese: "mozzarella",
+    sauce: "traditional red",
+    addToppings: function() {
+        console.log("Please add pepperoni and sausage")
+    }
+};
+    topping = pizza.addToppings();
+
+// console.log(function());
